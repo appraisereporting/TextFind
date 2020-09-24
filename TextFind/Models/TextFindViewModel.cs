1d32bb10c5deb0b4aa6a428ef4c1339ea64b586b
Brad <Brad@deathstar>
2020-09-24 12:16:46 +0800

    adorned attributes with Display Data Annotations and required

--- TextFind/Models/TextFindViewModel.cs
+++ TextFind/Models/TextFindViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TextFind.Models
 {
     public class TextFindViewModel
     {
+        [Display(Name = "Text")]
+        [Required]
         public string Text { get; set; } = "";
+        
+        [Display(Name = "SubText")]
+        [Required]
         public string SubText { get; set; } = "";
+        
+        [Display(Name = "Case Insensitive Search")]
         public bool CaseInsentitiveSearch { get; set; } = false;
+
+        [Display(Name = "Search Results")]
         public IReadOnlyList<int> Results { get; set; } = new List<int>();
     }
 }
