01a50de9e7a54870be39a4e68e5465506915406b
Brad <Brad@deathstar>
2020-09-24 09:11:25 +0800

    Added View Model with Inputs Text, SubText and output Results

---
+++ TextFind/Models/TextFindViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TextFind.Models
+{
+    public class TextFindViewModel
+    {
+        public string Text { get; set; }
+        public string SubText { get; set; }
+        public List<int> Results { get; set; }
+    }
+}
