93f521415a10442b7a3fdebb32cec0d744ba2b79
Brad <Brad@deathstar>
2020-09-24 10:21:31 +0800

    working View and Controller

--- TextFind/Controllers/HomeController.cs
+++ TextFind/Controllers/HomeController.cs
@@ -18,9 +18,28 @@ namespace TextFind.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(TextFindViewModel model)
         {
-            return View();
+            if ((model.Text != null) && (model.SubText != null))
+            {
+                int start = 0;
+                int end = model.Text.Length;
+                int find = 0;
+
+                while ((start <= end) && (find > -1))
+                {
+                    find = model.Text.IndexOf(model.SubText, start);
+                    if (find != -1)
+                    {
+                        model.Results.Add(find);
+
+                        //start position moved one character left - repeated characters in subText are valid eg looking for xx in xxx gives two
+                        start = find + 1;
+                    }
+                }
+            }
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
